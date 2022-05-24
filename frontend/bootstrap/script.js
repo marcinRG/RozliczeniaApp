0ed320b448f6aec8569caa93062e17703ce4177e
MarcinRG <marcin.guszpit@gmail.com>
2022-05-24 08:01:37 +0200

    +

---
+++ frontend/bootstrap/script.js
@@ -0,0 +1,20 @@
+const deleteButtons = document.querySelectorAll('.remove-button')
+
+const popUpMessage = document.querySelector('.modal .modal-body')
+const popUpRemoveButton = document.querySelector('.modal .action-remove')
+
+for (const deleteBtn of deleteButtons) {
+    deleteBtn.addEventListener('click', (event) => {
+        const btn = event.target;
+        const itemName = btn.getAttribute('data-name');
+        const itemId = btn.getAttribute('data-id');
+        popUpMessage.textContent = `Czy chcesz usunąć ${itemName} z bazy danych?`;
+        popUpRemoveButton.setAttribute('href',
+            `${window.location.pathname}?mode=remove&id_elem=${itemId}`);
+    });
+}
+
+
+console.log(deleteButtons)
+console.log(popUpMessage)
+console.log(popUpRemoveButton)
